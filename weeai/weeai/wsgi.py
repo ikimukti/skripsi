41bd298d124f305215a4f4bfd5572449b3f5bc02
Firmansyah Mukti Wijaya <iki.mukti@gmail.com>
2023-04-03 08:32:05 +0700

    initial django project

---
+++ weeai/weeai/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for weeai project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weeai.settings')
+
+application = get_wsgi_application()
